ee6f59c3b1b728361ae2f126c0d68890e94bc68e
fusionjack <fusionjack@fusionjack.com>
2013-09-20 20:53:18 +0200

    Revert "Disable antialazing"
    This reverts commit 371bb825ae33e222ba624219a9e0073d7fd643e8.

--- llvm-device-build.mk
+++ llvm-device-build.mk
@@ -9,7 +9,6 @@ LOCAL_CFLAGS :=	\
 	-W	\
 	-Wno-unused-parameter	\
 	-Wwrite-strings	\
-    -Wno-error=strict-aliasing \
 	$(LOCAL_CFLAGS)
 
 # The three inline options together reduce libbcc.so almost 1MB.
